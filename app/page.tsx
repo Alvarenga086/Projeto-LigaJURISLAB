0b6c59261772598bc3b53d8d6a4b210458f613be
Alvarenga086 <danielviror2002@gmail.com>
2026-05-26 12:08:24 -0300

    Atualizaçao de evento em tempo real

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ async function getEvents(): Promise<SheetEvent[]> {
     // INSIRA A SUA API DO SHEETDB ABAIXO
     // revalidate: 21600 define a atualização do cache para acontecer a cada 6 horas
     const response = await fetch('https://sheetdb.io/api/v1/m1eeq8ljwrfly', {
-      next: { revalidate: 7200 }
+      //next: { revalidate: 7200 }
     });
     
     if (!response.ok) return [];
